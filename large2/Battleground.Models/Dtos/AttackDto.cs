2d319b785785218ae6d7337cbdfb92c2796c6401
Hermann Gislason <hgislason78@gmail.com>
2022-09-27 23:04:23 +0000

    Attack, Battle, Player Dto

--- large2/Battleground.Models/Dtos/AttackDto.cs
+++ large2/Battleground.Models/Dtos/AttackDto.cs
@@ -2,4 +2,10 @@
 
 public class AttackDto
 {
+    public int Id { get; set; }
+    public bool Success { get; set; }
+    public int CriticalHit { get; set; }
+    public int Damage { get; set; }
+    public int BattlePokemonId { get; set; }
+    public int PokemonIdentifier { get; set; }
 }
