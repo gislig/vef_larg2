a6b22327bcc8648c67c5d2c20f28c5e1bc1fff96
Gisli Gudmundsson <gisli@iteggs.com>
2022-09-28 20:36:43 +0000

    GetAllPokemons og GetPokemonByName

--- large2/Battleground.Repositories/Implementations/PokemonRepository.cs
+++ large2/Battleground.Repositories/Implementations/PokemonRepository.cs
@@ -1,8 +1,30 @@
+using Battleground.Repositories.Entities;
 using Battleground.Repositories.Interfaces;
+using Newtonsoft.Json;
 
 namespace Battleground.Repositories.Implementations;
 
 public class PokemonRepository : IPokemonRepository
 {
+    // Gets all pokemons from the API and returns them as a model list Pokemon
+    public async Task<IEnumerable<Pokemon>?> GetAllPokemons()
+    {
+        // Gets all pokemons from https://pokemon-proxy-api.herokuapp.com/pokemons
+        HttpClient client = new HttpClient();
+        var url = "https://pokemon-proxy-api.herokuapp.com/pokemons";
+        var response = await client.GetAsync(url);
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<IEnumerable<Pokemon>>(content);
+    }
     
+    // Gets a pokemon by name from the API and returns it as a model Pokemon
+    public async Task<Pokemon?> GetPokemonByName(string name)
+    {
+        // Gets a pokemon by id from https://pokemon-proxy-api.herokuapp.com/pokemons/{name}
+        HttpClient client = new HttpClient();
+        var url = $"https://pokemon-proxy-api.herokuapp.com/pokemons/{name}";
+        var response = await client.GetAsync(url);
+        var content = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<Pokemon>(content);
+    }
 }
--- large2/Battleground.Repositories/Interfaces/IPokemonRepository.cs
+++ large2/Battleground.Repositories/Interfaces/IPokemonRepository.cs
@@ -1,6 +1,9 @@
+using Battleground.Repositories.Entities;
+
 namespace Battleground.Repositories.Interfaces;
 
 public interface IPokemonRepository
 {
-    
+    Task<IEnumerable<Pokemon>> GetAllPokemons();
+    Task<Pokemon> GetPokemonByName(string name);
 }
