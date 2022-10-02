597b78adf7d2888eefaa806705ad6361994f10e5
Sindrit17 <sindrit17@ru.is>
2022-10-02 17:15:26 +0000

    refactoring

--- large2/Battleground.Api/Program.cs
+++ large2/Battleground.Api/Program.cs
@@ -1,45 +1,34 @@
 using Battleground.Api.Schema;
-using Battleground.Repositories;
-using Battleground.Repositories.Entities;
-using Battleground.Repositories.Implementations;
-using Battleground.Repositories.Interfaces;
 using Battleground.Services.Implementations;
 using Battleground.Services.Interfaces;
 using GraphQL;
 using GraphQL.Types;
 using Microsoft.AspNetCore.Builder;
+using Battleground.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpClient<IPokemonRepository, PokemonRepository>();
-
-var config = builder.Configuration;
-var connString = config.GetConnectionString("BattlegroundConnectionString");
-builder.Services.AddDbContext<BattlegroundDbContext>(options => options.UseNpgsql(connString, x => x.MigrationsAssembly("Battleground.Api")));
-
 builder.Services.AddDefer();
 builder.Services.AddHttpScope();
+builder.Services.AddDbContext<BattlegroundDbContext>(options => 
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("BattlegroundConnectionString"), b => b.MigrationsAssembly("Battleground.Api"));
+});
 
 builder.Services.AddTransient<IPokemonService, PokemonService>();
 builder.Services.AddTransient<IPlayerService, PlayerService>();
 builder.Services.AddTransient<IBattleService, BattleService>();
 builder.Services.AddTransient<IInventoryService, InventoryService>();
-
-builder.Services.AddTransient<IPokemonRepository, PokemonRepository>();
-builder.Services.AddTransient<IPlayerRepository, PlayerRepository>();
-builder.Services.AddTransient<IAttackRepository, AttackRepository>();
-builder.Services.AddTransient<IPlayerInventoryRepository, PlayerInventoryRepository>();
-builder.Services.AddTransient<IBattleRepository, BattleRepository>();
-builder.Services.AddTransient<IBattlePlayerRepository, BattlePlayerRepository>();
-builder.Services.AddTransient<IBattlePokemonRepository, BattlePokemonRepository>();
-builder.Services.AddTransient<IBattleStatusRepository, BattleStatusRepository>();
+builder.Services.AddHttpClient<IPokemonService, PokemonService>(client => {
+    client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("PokemonApiBaseUrl"));
+});
 
 builder.Services.AddGraphQL(qlBuilder => qlBuilder
     .AddSystemTextJson()
     .AddErrorInfoProvider(opt => opt.ExposeExceptionDetails = true)
     .AddSchema<BattlegroundSchema>()
-    .AddGraphTypes(typeof(BattlegroundSchema).Assembly)
+    .AddGraphTypes()
     .AddDataLoader());
 
 var app = builder.Build();
