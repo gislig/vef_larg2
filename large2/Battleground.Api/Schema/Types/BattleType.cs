4495bb9704e26f78d85aab57191ee1e25f2df6fc
Gisli Gudmundsson <gisli@iteggs.com>
2022-10-05 20:30:58 +0000

    attacktype bugged

--- large2/Battleground.Api/Schema/Types/BattleType.cs
+++ large2/Battleground.Api/Schema/Types/BattleType.cs
@@ -9,16 +9,15 @@ namespace Battleground.Api.Schema.Types
         public BattleType()
         {
             Field(b => b.Id).Description("The ID of the battle.");
-            /*
+            
             Field(b => b.StatusId).Description("The status of the battle.");
             Field<PlayerType>("winner").Resolve(context => null);
             Field<NonNullGraphType<ListGraphType<NonNullGraphType<PokemonType>>>>("battlePokemons")
                 .Resolve(context => null);
             Field<NonNullGraphType<ListGraphType<NonNullGraphType<PlayerType>>>>("playersInMatch")
                 .Resolve(context => null);
-            Field<NonNullGraphType<ListGraphType<NonNullGraphType<AttackType>>>>("attacks")
-                .Resolve(context => null);
-                */            
+            //Field<NonNullGraphType<ListGraphType<NonNullGraphType<AttackType>>>>("attacks")
+            //    .Resolve(context => null);
         }
     }
 }
