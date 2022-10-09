bd613a7cd66be32da4f7a06402493bd29b8ab8a5
Sindrit17 <sindrit17@ru.is>
2022-10-09 13:39:19 +0000

    push

--- large2/Battleground.Api/Schema/Types/PokemonType.cs
+++ large2/Battleground.Api/Schema/Types/PokemonType.cs
@@ -9,6 +9,6 @@ public sealed class PokemonType : ObjectGraphType<PokemonDto>
         Field(x => x.name).Description("The name of the pokemon");
         Field(x => x.baseAttack).Description("The base attack of the pokemon");
         Field(x => x.healthPoints).Description("The health points of the pokemon");
-        Field(x => x.weight).Description("The weight of the pokemon");     
+        Field(x => x.weight).Description("The weight of the pokemon");
     }
 }
